1f2faadacc21845f8019bd4986fafbe9ceaf49c7
Andreas Eliasson <andreaseliasson91@gmail.com>
2021-11-26 13:20:41 +0100

    Re-named packages

--- application/dll/models/__init__.py
+++ application/dll/models/__init__.py
@@ -106,6 +106,9 @@ class Products(Base):
     def __repr__(self):
         return f"{self.product_id},{self.price_in}"
 
+    def __dict__(self):
+        return {'product_id': self.product_id, 'product_name': self.product_name, 'description': self.description, 'price_in': self.price_in}
+
 
 class ProductsFitModels(Base):
     __tablename__ = 'products_fits_models'
