39c6aedd3a025692004e4b9c8e6d179a5b2f862c
Katrina <89918645+katrinchzen@users.noreply.github.com>
2021-11-28 21:32:26 +0100

    Making small layout changes, trying to add multiple windows functions and tables

--- application/presentation/gui/gui.py
+++ application/presentation/gui/gui.py
@@ -1,33 +1,74 @@
 import PySimpleGUI as sg
 
-customerbutton = {'size': (10,2), 'font': ('Roboto Mono', 12), 'button_color': ("black", "#F8F8F8")}
-orderbutton = {'size': (10,2), 'font': ('Roboto Mono', 12), 'button_color': ("black", "#F8F8F8")}
-inventorybutton= {'size': (10,2), 'font': ('Roboto Mono', 12), 'button_color': ("black", "#F8F8F8")}
+button = {'size': (8, 2), 'font': ('Roboto Mono', 10), 'button_color': ("black", "#F8F8F8")}
+button2 = {'size': (5, 1), 'font': ('Roboto Mono', 8), 'button_color': ("black", "#474EAA")}
 
 
-# allt inuti fönstren
+def make_window1():
+    layout = [
+        [sg.Text('Rip and Ship', size=(30,1), justification='c',background_color="#54C5C9",
+                       text_color='black', font=('Arial', 30, 'bold'))],
+        [sg.Text('Search engine', size=(30,1), justification='c', background_color="#54C5C9",
+                       text_color='black', font=('Arial', 15, 'bold'))],
+        [sg.Text('        ', size=(30, 1), justification='c', background_color="#54C5C9",
+                       text_color='black', font=('Arial', 15, 'bold'))],
+        [sg.Button('Customers', **button), sg.Button('Orders', **button), sg.Button('Inventory', **button)]]
 
-layout = [
-    [sg.Text('Rip And Ship AB', size=(50,1), justification='c', background_color="#EDC7E3",
-     text_color='black', font=('Franklin Gothic Book', 30, 'bold'))],
-    [sg.Text('The worlds greatest search engine for our employees', size=(100,1), justification='c',
-             background_color="#EDC7E3",
-    text_color='black', font=('Franklin Gothic Book', 15,))],
-    [sg.Button('Customer',**customerbutton),
-     sg.Button('Order', **orderbutton), sg.Button('Inventory', **inventorybutton)],
-]
-
-# skapa fönstret
-window = sg.Window('Rip And Ship', layout=layout, background_color="#EDC7E3", size=(700, 700),
-                   grab_anywhere=True, element_justification='c')
+    return sg.Window('Rip and Ship', layout, finalize=True,
+                     background_color="#54C5C9", size=(500, 400), resizable=True, element_justification='center')
 
 
-# event loop för att processa events
+def make_window2():
+    data = []
+    header_list = []
+    layout = [[sg.Text('Customers', size=(30,1), justification='c', background_color="#54C5C9",
+               text_color='black', font=('Arial', 30, 'bold'))],
+              [sg.Button('Exit')],
+              [sg.Table(values=data,
+                        headings=header_list,
+                        auto_size_columns=False,
+                        num_rows=min(25, len(data)))]
+              ]
 
-while True:
-    event, values = window.read()
-    if event is None:
-        break
+    return sg.Window('Customers', layout, finalize=True,
+                     background_color="#54C5C9", size=(500, 400), resizable=True, element_justification='center')
 
-window.close()
 
+def make_window3():
+    layout = [
+        [sg.Text('Orders', size=(30,1), justification='c', background_color="#54C5C9",
+                 text_color='black', font=('Arial', 30, 'bold'))],
+        [sg.Button('Exit')]
+    ]
+    return sg.Window('Orders', layout, finalize=True)
+
+
+def make_window_4():
+    pass
+
+
+def main():
+    # Design pattern 1 - First window does not remain active
+    # window2 = None
+    # window1 = make_window1()
+
+    window1, window2, window3 = make_window1(), None, None
+
+    while True:
+        window, event, values = sg.read_all_windows()
+        if event == sg.WIN_CLOSED and window == window1:
+            break
+
+        if event == 'Customers' and not window2:
+            window1.hide()
+            window2 = make_window2()
+
+        if window == window2 and (event in (sg.WIN_CLOSED, 'Exit')):
+            window2.close()
+            window2 = None
+            window1.un_hide()
+    window1.close()
+
+
+if __name__ == '__main__':
+    main()
