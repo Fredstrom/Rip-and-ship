c8276e9cb950160b460154a94e9dd1b9389a9466
Katrina <89918645+katrinchzen@users.noreply.github.com>
2021-11-29 12:59:59 +0100

    Fixing window "orders" and while True - loop

--- application/presentation/gui/gui.py
+++ application/presentation/gui/gui.py
@@ -1,13 +1,13 @@
 import PySimpleGUI as sg
 
-button = {'size': (8, 2), 'font': ('Roboto Mono', 10), 'button_color': ("black", "#F8F8F8")}
+button = {'size': (10, 2), 'font': ('Roboto Mono', 10, 'bold'), 'button_color': ("white", "#000000")}
 button2 = {'size': (5, 1), 'font': ('Roboto Mono', 8), 'button_color': ("black", "#474EAA")}
 
 
-def make_window1():
+def frontpage_window():
     layout = [
         [sg.Text('Rip and Ship', size=(30,1), justification='c',background_color="#54C5C9",
-                       text_color='black', font=('Arial', 30, 'bold'))],
+                       text_color='black', font=('Helvetica', 30, 'bold'))],
         [sg.Text('Search engine', size=(30,1), justification='c', background_color="#54C5C9",
                        text_color='black', font=('Arial', 15, 'bold'))],
         [sg.Text('        ', size=(30, 1), justification='c', background_color="#54C5C9",
@@ -18,7 +18,7 @@ def make_window1():
                      background_color="#54C5C9", size=(500, 400), resizable=True, element_justification='center')
 
 
-def make_window2():
+def customer_window():
     data = []
     header_list = []
     layout = [[sg.Text('Customers', size=(30,1), justification='c', background_color="#54C5C9",
@@ -34,17 +34,30 @@ def make_window2():
                      background_color="#54C5C9", size=(500, 400), resizable=True, element_justification='center')
 
 
-def make_window3():
+def order_window():
+    data = []
+    header_list = []
     layout = [
         [sg.Text('Orders', size=(30,1), justification='c', background_color="#54C5C9",
                  text_color='black', font=('Arial', 30, 'bold'))],
+        [sg.Button('Exit')],
+        [sg.Table(values=data,
+                  headings=header_list,
+                  auto_size_columns=False,
+                  num_rows=min(25, len(data)))]
+    ]
+    return sg.Window('Orders', layout, finalize=True,
+                     background_color="#54C5C9", size=(500, 400), resizable=True, element_justification='center')
+
+
+def inventory_window():
+    layout = [
+        [sg.Text('Inventory', size=(30, 1), justification='c', background_color="#54C5C9",
+                  text_color='black', font=('Arial', 30, 'bold'))],
         [sg.Button('Exit')]
     ]
-    return sg.Window('Orders', layout, finalize=True)
-
-
-def make_window_4():
-    pass
+    return sg.Window('Inventory', layout, finalize=True,
+                      background_color="#54C5C9", size=(500, 400), resizable=True, element_justification='center')
 
 
 def main():
@@ -52,7 +65,7 @@ def main():
     # window2 = None
     # window1 = make_window1()
 
-    window1, window2, window3 = make_window1(), None, None
+    window1, window2, window3, window4 = frontpage_window(), None, None, None
 
     while True:
         window, event, values = sg.read_all_windows()
@@ -61,12 +74,31 @@ def main():
 
         if event == 'Customers' and not window2:
             window1.hide()
-            window2 = make_window2()
+            window2 = customer_window()
 
         if window == window2 and (event in (sg.WIN_CLOSED, 'Exit')):
             window2.close()
             window2 = None
             window1.un_hide()
+
+        if event == 'Orders' and not window3:
+            window1.hide()
+            window3 = order_window()
+
+        if window == window3 and (event in (sg.WIN_CLOSED, 'Exit')):
+            window3.close()
+            window3 = None
+            window1.un_hide()
+
+        if window == 'Inventory' and not window4:
+            window1.hide()
+            window4 = inventory_window()
+
+        if window == window4 and (event in (sg.WIN_CLOSED, 'Exit')):
+            window4.close()
+            window4 = None
+            window1.un_hide()
+
     window1.close()
 
 
